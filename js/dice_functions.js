6a4c63575a52ae7108a7b26ce2c27cd8fe9d4057
CoconutTank <sloser@gmail.com>
2022-12-05 11:44:58 -0800

    Update dice_functions.js
    Add another button function (this time for dynamic page changes).

--- js/dice_functions.js
+++ js/dice_functions.js
@@ -3,10 +3,21 @@ function helloWorld() {
   console.log("Hello World (via js)!");
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('hello_world_button');
+function createParagraph() {
+  const para = document.createElement('p');
+  para.textContent = 'You clicked a button!';
+  document.body.appendChild(para);
+}
 
-  for (const button of buttons) {
+document.addEventListener('DOMContentLoaded', () => {
+  const hello_world_buttons = document.querySelectorAll('hello_world_button');
+  const paragraph_buttons = document.querySelectorAll('button');
+
+  for (const button of hello_world_buttons) {
     button.addEventListener('click', helloWorld);
+  }
+
+  for (const button of paragraph_buttons) {
+    button.addEventListener('click', createParagraph);
   }
 });  
