0008d2814ffde7c88fa97cbb2465d0cab932a92b
CoconutTank <sloser@gmail.com>
2022-12-05 11:34:01 -0800

    Update dice_functions.js
    Move html changes into js script.

--- js/dice_functions.js
+++ js/dice_functions.js
@@ -1,3 +1,12 @@
-function helloWorld () {
+
+function helloWorld() {
   console.log("Hello World (via js)");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('button');
+
+  for (const button of buttons) {
+    button.addEventListener('click', helloWorld);
+  }
+});  
