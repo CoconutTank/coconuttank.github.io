de51c872f54797911820a30ecfdc1bf4a228e6b8
CoconutTank <sloser@gmail.com>
2022-12-05 11:39:27 -0800

    Update dice_functions.js
    More updates (test to see if id works with query selector all, or if only keywords get filtered).

--- js/dice_functions.js
+++ js/dice_functions.js
@@ -1,10 +1,10 @@
 
 function helloWorld() {
-  console.log("Hello World (via js)");
+  console.log("Hello World (via js)!");
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('button');
+  const buttons = document.querySelectorAll('hello_world_button');
 
   for (const button of buttons) {
     button.addEventListener('click', helloWorld);
